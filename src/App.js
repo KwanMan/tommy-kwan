dd2eca6f8395745339910676eda90429a54ea39d
Tommy Kwan <tkwan196@gmail.com>
2018-01-09 22:59:11 +0000

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Background from './Background'
+import HomePage from './HomePage'
+
+import './App.css'
 
 export default function App () {
   return (
     <div className='App'>
       <Background />
-      I. Am. Alive.
+      <HomePage />
     </div>
   )
 }
---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './HomePage.css'
+
+export default function HomePage () {
+  return (
+    <div className='HomePage'>
+      <div className='HomePage-hey'>Hey there, I'm</div>
+      <div className='HomePage-name'>Tommy Kwan</div>
+      <div className='HomePage-tagline'>let me tell you my story...</div>
+      <div className='HomePage-soon'>...soon</div>
+    </div>
+  )
+}

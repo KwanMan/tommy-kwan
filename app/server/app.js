9fd66fcdfd08c9188a781c4be5b4395fadc39375
Tommy Kwan <tkwan196@gmail.com>
2018-02-17 13:37:03 +0000

    Expose dist folder

--- app/server/app.js
+++ app/server/app.js
@@ -13,6 +13,7 @@ createChat(server)
 
 dev(app)
 app.use('/assets', express.static(path.resolve(__dirname, '../assets')))
+app.use('/dist', express.static(path.resolve(__dirname, '../dist')))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../assets/index.html'))
 })
